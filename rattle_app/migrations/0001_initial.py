4e25a00bc72e5a81ed6379b1f7dc09ebabe0b7d5
Fei Gao <fgao22@emory.edu>
2015-01-24 16:57:54 -0500

    Created UserProfile and Rattle Object in Models Migrate to database

---
+++ rattle_app/migrations/0001_initial.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Rattle',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('content', models.CharField(max_length=140)),
+                ('creation_date', models.DateTimeField(auto_now=True)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('follows', models.ManyToManyField(related_name='followed_by', to='rattle_app.UserProfile')),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- rattle_app/models.py
+++ rattle_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+import hashlib
 
 # Create your models here.
+class Rattle(models.Model):
+    content = models.CharField(max_length=140)
+    user = models.ForeignKey(User)
+    creation_date = models.DateTimeField(auto_now=True, blank=True)
+ 
+ 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
+ 
+    def gravatar_url(self):
+        return "http://www.gravatar.com/avatar/%s?s=50" % hashlib.md5(self.user.email).hexdigest()
+ 
+ 
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
