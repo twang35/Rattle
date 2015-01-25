f9225b0f37953d2310d6f7ef58454ae96a045183
Tony Wang <twang35@outlook.com>
2015-01-25 15:59:14 -0500

    Small typo, runserver works now

--- rattle_app/forms.py
+++ rattle_app/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from django.utils.html import strip_tags
-from Rattle_app.models import Rattle
+from rattle_app.models import Rattle
 
 class UserCreateForm(UserCreationForm):
     email = forms.EmailField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder': 'Email'}))
