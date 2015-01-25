a0628e036c48a9ea0e0b29c0761f211a36c7a65e
Fei Gao <fgao22@emory.edu>
2015-01-25 15:31:59 -0500

    Changing typos in views.py

--- rattle_app/views.py
+++ rattle_app/views.py
@@ -4,22 +4,22 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
-from ribbit_app.forms import AuthenticateForm, UserCreateForm, RibbitForm
-from ribbit_app.models import Ribbit
+from rattle_app.forms import AuthenticateForm, UserCreateForm, RattleForm
+from rattle_app.models import Rattle
 
 def index(request, auth_form=None, user_form=None):
     # User is logged in
     if request.user.is_authenticated():
-        ribbit_form = RibbitForm()
+        rattle_form = RattleForm()
         user = request.user
-        ribbits_self = Ribbit.objects.filter(user=user.id)
-        ribbits_buddies = Ribbit.objects.filter(user__userprofile__in=user.profile.follows.all)
-        ribbits = ribbits_self | ribbits_buddies
+        rattles_self = Rattle.objects.filter(user=user.id)
+        rattles_buddies = Rattle.objects.filter(user__userprofile__in=user.profile.follows.all)
+        rattles = rattles_self | rattles_buddies
  
         return render(request,
                       'buddies.html',
-                      {'ribbit_form': ribbit_form, 'user': user,
-                       'ribbits': ribbits,
+                      {'rattle_form': rattle_form, 'user': user,
+                       'rattles': rattles,
                        'next_url': '/', })
     else:
         # User is not logged in
